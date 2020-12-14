0ba89049f55063966b6eb2e957396317fc28d18e
NickSpangler <Spangleyman@mac.com>
2020-12-14 15:09:06 -0500

    API iteration set in seeds.rb

--- kbb-backend/db/seeds.rb
+++ kbb-backend/db/seeds.rb
@@ -1,22 +1,44 @@
 def getData
     Movie.all.destroy_all
+    Actor.all.destroy_all
+    MovieActor.all.destroy_all
 
-    i = 0
-    while i <= 20 do
+    i = 1
+    while i <= 1000 do
         response = RestClient.get("https://api.themoviedb.org/3/movie/#{i}?api_key=f561f9632b29613ae5d1646d3298a753&language=en-US"){|response, request, result| response }
-        json = JSON.parse response
-
-        if !json.nil? && json['original_language'] == 'en'
-            Movie.create(
-                tmdb_id: json['id'],
-                imdb_id: json['imdb_id'],
-                title: json['title'],
-                poster_path: json['poster_path'],
-                release_year: json['release_date'].split('-').first
-            )
+        if response.code == 200
+            json = JSON.parse response
+            if json['original_language'] == 'en'
+                movie = Movie.create(
+                    tmdb_id: json['id'],
+                    imdb_id: json['imdb_id'],
+                    title: json['title'],
+                    poster_path: json['poster_path'],
+                    release_year: json['release_date'].split('-').first
+                )
+                                    
+                castResponse = RestClient.get("https://api.themoviedb.org/3/movie/#{i}/credits?api_key=f561f9632b29613ae5d1646d3298a753&language=en-US")
+                castJson = JSON.parse castResponse
+                castJson['cast'].map do |person|
+                    if actor = Actor.find_by(tmdb_id: person['id'])
+                        MovieActor.create(movie_id: movie.id, actor_id: actor.id, character: person['character'])
+                    else
+                        actor = Actor.create(
+                            tmdb_id: person['id'],
+                            name: person['name'],
+                            profile_path: person['profile_path'],
+                            gender: person['gender']
+                        )
+                        MovieActor.create(movie_id: movie.id, actor_id: actor.id, character: person['character'])
+                    end
+                end
+            end
+            puts json['title']
+        else
+            puts response.code
         end
         i += 1
     end
 end
 
-getData()   
+# getData()   
