fc8ee4e3b5be7c1e02e9c6f4264cd147593e77c0
NickSpangler <Spangleyman@mac.com>
2020-12-14 10:58:08 -0500

    create Movie resource

---
+++ kbb-backend/db/migrate/20201214155532_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.integer :tmdb_id
+      t.integer :imdb_id
+      t.string :title
+      t.string :poster_path
+      t.integer :release_year
+
+      t.timestamps
+    end
+  end
+end
