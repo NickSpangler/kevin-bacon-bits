e3e752c2e2ba2d15df98ad0c8b67bc5f6e342d5a
NickSpangler <Spangleyman@mac.com>
2020-12-16 10:56:40 -0500

    create TargetAInput component

--- frontend/src/components/six-degrees-search/SixDegreesSearch.js
+++ frontend/src/components/six-degrees-search/SixDegreesSearch.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import TargetAInput from './TargetAInput'
 
 export default function SixDegreesSearch() {
     return (
         <div>
             <h1>Search for the link between two actors:</h1>
+            <TargetAInput/>
         </div>
     )
 }
