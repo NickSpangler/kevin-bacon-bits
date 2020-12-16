35a474413c1ff4736f2b07c647339704a5072b01
NickSpangler <Spangleyman@mac.com>
2020-12-15 19:59:35 -0500

    add ILIKE to actor scope :auto_complete

--- kbb-backend/app/controllers/actors_controller.rb
+++ kbb-backend/app/controllers/actors_controller.rb
@@ -1,7 +1,7 @@
 class ActorsController < ApplicationController
 
     def auto_complete
-        actors = Actor.auto_complete(params[:auto_complete]).limit(10)
+        actors = Actor.auto_complete(params[:input]).limit(10)
         render json: actors
     end
 
--- kbb-backend/app/models/actor.rb
+++ kbb-backend/app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ApplicationRecord
     has_many :movie_actors
     has_many :movies, :through => :movie_actors
-    
-    scope :auto_complete, -> (query) { where("name LIKE (?)", "%#{query}%" )}
+
+    scope :auto_complete, -> (query) { where("name ILIKE (?)", "%#{query}%" )}
 end
