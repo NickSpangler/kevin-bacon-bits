1d7c50d5dca64ddcf00bee7274825a0b2ed8873a
NickSpangler <Spangleyman@mac.com>
2020-12-15 19:46:27 -0500

    enable CORS

--- kbb-backend/app/controllers/actors_controller.rb
+++ kbb-backend/app/controllers/actors_controller.rb
@@ -1,2 +1,8 @@
 class ActorsController < ApplicationController
+
+    def auto_complete
+        actors = Actor.limit(10)
+        render json: actors
+    end
+    
 end
--- kbb-backend/config/routes.rb
+++ kbb-backend/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get '/actors/auto_complete', to: 'actors#auto_complete'
+  
   resources :movie_actors
   resources :actors
   resources :movies
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
