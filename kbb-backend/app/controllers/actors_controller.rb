a134020c1bf98a87274163e186d15dd258c34c62
NickSpangler <Spangleyman@mac.com>
2020-12-15 19:52:09 -0500

    add SCOPE :auto_complete to actor.rb

--- kbb-backend/app/controllers/actors_controller.rb
+++ kbb-backend/app/controllers/actors_controller.rb
@@ -1,8 +1,12 @@
 class ActorsController < ApplicationController
 
     def auto_complete
+
+        @shows = Show.search(params[:search])
+
+
         actors = Actor.limit(10)
         render json: actors
     end
-    
+
 end
--- kbb-backend/app/models/actor.rb
+++ kbb-backend/app/models/actor.rb
@@ -1,4 +1,6 @@
 class Actor < ApplicationRecord
     has_many :movie_actors
     has_many :movies, :through => :movie_actors
+    
+    scope :auto_complete, -> (query) { where("name LIKE (?)", "%#{query}%" )}
 end
