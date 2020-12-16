bb09003515fe4a1fd359fcc6481c79c2cec80362
NickSpangler <Spangleyman@mac.com>
2020-12-16 14:56:48 -0500

    Alphabetized actor auto_complete

--- kbb-backend/app/models/actor.rb
+++ kbb-backend/app/models/actor.rb
@@ -2,5 +2,5 @@ class Actor < ApplicationRecord
     has_many :movie_actors
     has_many :movies, :through => :movie_actors
 
-    scope :auto_complete, -> (query) { where("name ILIKE (?)", "%#{query}%" )}
+    scope :auto_complete, -> (query) { where("name ILIKE (?)", "#{query}%" ).order(:name)}
 end
