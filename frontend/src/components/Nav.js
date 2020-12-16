1ad10c134a13e3b7a63389fd8b8b86a32787f48c
NickSpangler <Spangleyman@mac.com>
2020-12-16 10:38:54 -0500

    implement antd css to Nav component

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,15 +1,16 @@
+import '../css/Nav.css';
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <div>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/six-degrees-search'>Six Degrees Search</Link></li>
-                <li><Link to='/six-degrees-challenge'>Six Degrees Challenge</Link></li>
-                <li><Link to='/filmography-challenge'>Filmography Challenge</Link></li>
-            </ul>
+        <div className="demo">
+            <div className="demo-nav">
+                <Link to='/'>Home</Link>
+                <Link to='/six-degrees-search'>Six Degrees Search</Link>
+                <Link to='/six-degrees-challenge'>Six Degrees Challenge</Link>
+                <Link to='/filmography-challenge'>Filmography Challenge</Link>
+            </div>
         </div>
     )
 }
