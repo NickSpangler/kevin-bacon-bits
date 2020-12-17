d200275bc1bbb7930492f0b40499a7224d332bdd
NickSpangler <Spangleyman@mac.com>
2020-12-17 10:52:40 -0500

    add search button to sixdegreessearch

--- frontend/src/components/six-degrees-search/SearchResults.js
+++ frontend/src/components/six-degrees-search/SearchResults.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SearchResults() {
     return (
         <div>
-            <h1>This is where search results go</h1>
+            <h1>This is where search results go here</h1>
         </div>
     )
 }
--- frontend/src/components/six-degrees-search/SixDegreesSearch.js
+++ frontend/src/components/six-degrees-search/SixDegreesSearch.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { Button } from 'antd'
+import { SearchOutlined } from '@ant-design/icons';
 import TargetAInput from './TargetAInput'
 import TargetBInput from './TargetBInput'
 import SearchResults from './SearchResults'
 
 export default function SixDegreesSearch() {
+    
     return (
         <div>
             <h1>Search for the link between two actors:</h1>
             <TargetAInput />
             <br/><br/>
             <TargetBInput />
+            <br/><br/>
+            <Button type="primary" icon={<SearchOutlined />}>
+                Search
+            </Button>
             <br/><br/>
             <SearchResults />
         </div>
