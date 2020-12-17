6f6418d1115a2cfc7a4bb05a0bb9e2156abe9a9f
NickSpangler <Spangleyman@mac.com>
2020-12-17 11:26:38 -0500

    define actions in search reducer

--- frontend/src/redux/reducers/searchReducer.js
+++ frontend/src/redux/reducers/searchReducer.js
@@ -1,5 +1,14 @@
-function searchReducer(state = { targetA: '', targetB: '', results: [] }, action) {
-
+function searchReducer(state = { target_A: '', target_B: '', results: [] }, action) {
+    switch(action.type) {
+        case 'UPDATE_TARGET_A':
+            return { ...state, target_A: action.payload };
+        case 'UPDATE_TARGET_B':
+            return { ...state, target_B: action.payload };
+        case 'UPDATE_RESULTS':
+            return {...state, results: action.payload };
+        default:
+            return state;
+    }
 }
 
 export default searchReducer
