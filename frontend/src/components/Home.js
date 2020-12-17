0ad1419fb7a10f5d36402744cc1c24f010a589a3
NickSpangler <Spangleyman@mac.com>
2020-12-16 21:31:46 -0500

    Add welcome text

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Typography, Divider } from 'antd';
+
+const { Title, Paragraph, Text} = Typography;
+
 
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome to Kevin Bacon Bits!</h1>
-        </div>
+        <Typography>
+            <Title>Welcome to Kevin Bacon Bits</Title>
+            <Paragraph>
+                At KBB we know the bits are just good as the whole strip... if not better!
+                So why not have a taste?
+            </Paragraph>
+            <Paragraph>
+                In <Text strong underline>Six Degrees Search</Text> you can search any two actors in our database 
+                and see if they can be connected with six degrees of separation or less.
+            </Paragraph>
+            <Paragraph>
+                The <Text strong underline>Six Degrees Challenge</Text> let's you choose an actor and a number of 
+                degrees you want to be challenged by, and we'll tell you who to connect them with!
+            </Paragraph>
+            <Paragraph>
+                And the <Text strong underline>Filmography Challenge</Text> let's you choose an actor and we'll 
+                test your knowledge of their filmography by making you select posters from the films they've been in. Good luck!
+            </Paragraph>
+        </Typography>
     )
 }
