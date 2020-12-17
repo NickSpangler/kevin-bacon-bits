47702168cf2db9ad4023642dada25002b8e1d3c0
NickSpangler <Spangleyman@mac.com>
2020-12-17 11:51:03 -0500

    create UPDATE_RESULTS action in searchActions

---
+++ frontend/src/redux/actions/searchActions.js
@@ -0,0 +1,9 @@
+export const getResults = () => {
+    return (dispatch) => {
+        fetch('http://localhost:3000/search_for_link')
+        .then(res => res.json())
+        .then(data => {
+            dispatch({type: 'UPDATE_RESULTS', payload: data})
+        })
+    }
+}
