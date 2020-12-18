6d96db82d56962d4be89e7216451b7658c0fae60
NickSpangler <Spangleyman@mac.com>
2020-12-18 14:25:51 -0500

    define filmographyReducer

---
+++ frontend/src/redux/reducers/filmographyReducer.js
@@ -0,0 +1,15 @@
+function filmographyReducer(state = {
+                                actor: {},
+                                actor_movies: [],
+                                current_movie: {},
+                                possible_answers: [],
+                                round_result: 'waiting',
+                                history: []
+                            }, action) {
+    switch(action.type) {
+        case 'SET_ACTOR':
+            return { ...state, actor: action.actor, actor_movies: action.actor_movies }
+        default:
+            return state;
+    }
+}
