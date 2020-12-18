119689e7fe6019b8a5d2d0305c4e9b1c01c2eecb
NickSpangler <Spangleyman@mac.com>
2020-12-18 11:32:42 -0500

    add conditional render to searchResults

--- frontend/src/components/six-degrees-search/SearchResults.js
+++ frontend/src/components/six-degrees-search/SearchResults.js
@@ -2,9 +2,18 @@ import React from 'react'
 import Level from './Level'
 
 export default function SearchResults(props) {
-    return (
-        <div>
-            {props.results.map( level => <Level data={level} />)}
-        </div>
-    )
+    if (props.loading === true) {
+        return (
+            <div>
+                <p>Searching for link...</p>
+            </div>
+        )
+
+    } else {
+        return (
+            <div>
+                {props.results.map( level => <Level data={level} />)}
+            </div>
+        )
+    }
 }
