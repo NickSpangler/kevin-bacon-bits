69b65a7ed8c3a511a579107df449c9d842df24d7
NickSpangler <Spangleyman@mac.com>
2020-12-18 12:24:33 -0500

    before styled components

--- frontend/src/components/six-degrees-search/SearchResults.js
+++ frontend/src/components/six-degrees-search/SearchResults.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import Level from './Level'
+import React from 'react';
+import Level from './Level';
+import { Ring } from "react-awesome-spinners";
 
 export default function SearchResults(props) {
     if (props.loading === true) {
         return (
             <div>
+                <Ring />
                 <p>Searching for link...</p>
             </div>
         )
