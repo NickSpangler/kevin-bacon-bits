ee7e486fd9b7c7eb9937f242886afd39e92704e0
NickSpangler <Spangleyman@mac.com>
2020-12-18 16:55:25 -0500

    define get_possible_movies action in movies_controller

--- kbb-backend/app/controllers/movies_controller.rb
+++ kbb-backend/app/controllers/movies_controller.rb
@@ -1,2 +1,8 @@
 class MoviesController < ApplicationController
+
+    def get_possible_movies
+        actor = Actor.find(params[:actor_id])
+        possible_ movies = Movie.find_by_year(params[:year]).filter( |m| !m.actors.include?(actor)).shuffle.slice(0, 4)
+        render json: possible_movies
+    end
 end
