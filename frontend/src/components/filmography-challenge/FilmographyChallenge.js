31592dbb7b821848b23654c182a519c3345d0172
NickSpangler <Spangleyman@mac.com>
2020-12-18 13:14:54 -0500

    create ActorSelect component

--- frontend/src/components/filmography-challenge/FilmographyChallenge.js
+++ frontend/src/components/filmography-challenge/FilmographyChallenge.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ActorSelect from './ActorSelect'
 
 export default function FilmographyChallenge() {
     return (
         <div>
             <h1>How well do you know your favorite actor's career?</h1>
+            <ActorSelect />
         </div>
     )
 }
