cf3d8345851b922b3e5636b3c0a001fad823df00
NickSpangler <Spangleyman@mac.com>
2020-12-18 22:33:01 -0500

    import Space from antd

--- frontend/src/components/filmography-challenge/OptionsContainer.js
+++ frontend/src/components/filmography-challenge/OptionsContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Space, Upload, Popconfirm } from 'antd';
 
 const OptionsContainer = (props) => {
     if (props.possible_answers === 'between rounds') {
@@ -7,7 +8,9 @@ const OptionsContainer = (props) => {
     )} else {
         return (
             <>
-            {props.possible_answers.map( option => <img src={`https://image.tmdb.org/t/p/w200${option.poster_path}`}></img>)}
+                <Space>
+                    {props.possible_answers.map( option => <img src={`https://image.tmdb.org/t/p/w200${option.poster_path}`}></img>)}
+                </Space>
             </>
         )
     }
