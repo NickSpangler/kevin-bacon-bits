430108b32f2ff9fcb930a33115c72149afeefaa4
NickSpangler <Spangleyman@mac.com>
2020-12-18 19:06:16 -0500

    first round possible answers load correctly

--- frontend/src/redux/reducers/filmographyReducer.js
+++ frontend/src/redux/reducers/filmographyReducer.js
@@ -11,8 +11,13 @@ function filmographyReducer(state = {
             let first_movie = action.actor_movies[Math.floor(Math.random()*state.actor_movies.length)]
             return { ...state, actor: action.actor, actor_movies: action.actor_movies, current_movie: first_movie }
         case 'GET_POSSIBLE_MOVIES':
-            let movies = [...action.movies, state.current_movie]
-            return {...state, possible_answers: movies }
+            let index = Math.floor(Math.random()*4)
+            let movies = action.movies
+            movies.splice(index, 0, state.current_movie)
+            return {
+                ...state, 
+                possible_answers: movies,
+                actor_movies: state.actor_movies.filter(m => m.id !== state.current_movie.id)}
         default:
             return state;
     }
