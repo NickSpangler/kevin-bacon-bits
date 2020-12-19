fc4656a97baea683b27476eb4ac96b3e809f5725
NickSpangler <Spangleyman@mac.com>
2020-12-19 17:26:30 -0500

    fix nil release_year issue in getPossibleMovies

--- kbb-backend/app/controllers/movies_controller.rb
+++ kbb-backend/app/controllers/movies_controller.rb
@@ -5,11 +5,11 @@ class MoviesController < ApplicationController
         
         current_movie = Movie.find(params[:current_movie])
 
-        cast = current_movie.actors[0...15].filter{ |a| a.id != actor.id}
+        cast = current_movie.actors[0...15].filter{ |a| a.id != actor.id }
         movies_that_year = []
         cast.each do |a|
             a.movies.each do |m|
-                if m.release_year >= current_movie.release_year - 1 && m.release_year <= current_movie.release_year + 1 && !m.actors.include?(actor)
+                if m.release_year != nil && m.release_year >= current_movie.release_year - 2 && m.release_year <= current_movie.release_year + 2 && !m.actors.include?(actor)
                     movies_that_year << m
                 end
             end
