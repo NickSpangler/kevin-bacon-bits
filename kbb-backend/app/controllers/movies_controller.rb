df570d591ac70cc3cb911031fb6c02e103149a4a
NickSpangler <Spangleyman@mac.com>
2020-12-19 16:45:19 -0500

    algorithm selects movies +- one year from current_movies

--- kbb-backend/app/controllers/movies_controller.rb
+++ kbb-backend/app/controllers/movies_controller.rb
@@ -2,14 +2,14 @@ class MoviesController < ApplicationController
 
     def possible_movies
         actor = Actor.find(params[:actor_id])
-        # movies_that_year = Movie.find_by_year(params[:year]).shuffle.slice(0,10)
+        
         current_movie = Movie.find(params[:current_movie])
-        # possible_movies = movies_that_year.filter{ |m| !m.actors.include?(actor)}.slice(0,4)
+
         cast = current_movie.actors[0...15].filter{ |a| a.id != actor.id}
         movies_that_year = []
         cast.each do |a|
             a.movies.each do |m|
-                if m.release_year == current_movie.release_year && !m.actors.include?(actor)
+                if m.release_year >= current_movie.release_year - 1 && m.release_year <= current_movie.release_year - 1 && !m.actors.include?(actor)
                     movies_that_year << m
                 end
             end
