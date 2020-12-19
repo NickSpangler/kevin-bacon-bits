f47655bb8b6588086bc469e78455c4287ad184b1
NickSpangler <Spangleyman@mac.com>
2020-12-18 22:22:20 -0500

    when between rounds, nothing rendered

--- frontend/src/components/filmography-challenge/OptionsContainer.js
+++ frontend/src/components/filmography-challenge/OptionsContainer.js
@@ -3,9 +3,7 @@ import React from 'react'
 const OptionsContainer = (props) => {
     if (props.possible_answers === 'between rounds') {
         return (
-            <div>
-                <p>No options yet.</p>
-            </div>
+            <></>
     )} else {
         return (
             <p>Now there are options.</p>
