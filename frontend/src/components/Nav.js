2325a8164a57ae735d22af6e5b77b0662d17bc40
NickSpangler <Spangleyman@mac.com>
2020-12-19 16:53:11 -0500

    import Badge to NavBar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,6 +1,7 @@
 import '../css/Nav.css';
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Badge } from 'antd'
 
 export default function Nav() {
     return (
@@ -9,7 +10,9 @@ export default function Nav() {
                 <Link to='/'>Home</Link>
                 <Link to='/six-degrees-search'>Six Degrees Search</Link>
                 <Link to='/six-degrees-challenge'>Six Degrees Challenge</Link>
+                <Badge count={0} showZero offset={[2, 15]} size="small">
                 <Link to='/filmography-challenge'>Filmography Challenge</Link>
+                </Badge>
             </div>
         </div>
     )
