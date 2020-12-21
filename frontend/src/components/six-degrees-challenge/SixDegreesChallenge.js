ac75c8d93686881a851608497b222e65cdd0580b
NickSpangler <Spangleyman@mac.com>
2020-12-21 10:27:30 -0500

    import 'coming_soon' result element from antd

--- frontend/src/components/six-degrees-challenge/SixDegreesChallenge.js
+++ frontend/src/components/six-degrees-challenge/SixDegreesChallenge.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Result, Button } from 'antd';
 
 export default function SixDegreesChallenge() {
     return (
-        <div>
-            <h1>Can you find the link?</h1>
-        </div>
+        <Result
+            status="404"
+            title="Sorry, we're still unboxing this feature."
+            subTitle="Check back later for updates, and enjoy another feature at this time."
+            extra={<Button type="primary">Back Home</Button>}
+        />
     )
 }
