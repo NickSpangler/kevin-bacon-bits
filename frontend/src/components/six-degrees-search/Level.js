d348cd52be502dda5a6f6bd6fa3467602fd50dae
NickSpangler <Spangleyman@mac.com>
2020-12-23 17:13:59 -0500

    add classes to Level component

--- frontend/src/components/six-degrees-search/Level.js
+++ frontend/src/components/six-degrees-search/Level.js
@@ -9,8 +9,9 @@ export default function Level(props) {
     const degrees = props.degree === 1 ? "Degree" : "Degrees"
     const target_a_path = props.data.target_a.profile_path === null ? silhouette : (`https://image.tmdb.org/t/p/w200${props.data.target_a.profile_path}`)
     const target_b_path = props.data.target_b.profile_path === null ? silhouette : (`https://image.tmdb.org/t/p/w200${props.data.target_b.profile_path}`)
+    const level_class = props.degree % 2 === 0 ? 'slide-in-right' : 'slide-in-left'
     return (
-        <div>
+        <div className={level_class}>
             <Divider orientation="center">{`${props.degree} ${degrees}`}</Divider>
             <Row gutter={20} type="flex" align="middle">
                 <Col className="gutter-row" span={4} offset={4}>
