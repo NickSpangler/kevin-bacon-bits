84f58dd94c69abb72a3b4d938176e70d262e651f
NickSpangler <Spangleyman@mac.com>
2020-12-24 00:40:08 -0500

    edit Home component text

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -22,20 +22,17 @@ export default function Home() {
                         <br></br>
             <Typography>
                 <Title>Welcome to Kevin Bacon Bits</Title>
+                <br></br>
                 <Paragraph>
-                    At KBB we know the bits are just good as the whole strip... if not better!
-                    So why not have a taste?
-                </Paragraph>
-                <Paragraph>
-                    <Text strong underline>Six Degrees Search</Text> you can search any two actors in our database 
+                    <Text strong underline>Six Degrees Search:</Text> search any two actors in our database 
                     and see if they can be connected with six degrees of separation or less.
                 </Paragraph>
                 <Paragraph>
-                    <Text strong underline>Six Degrees Challenge</Text> lets you choose an actor and a number of 
+                    <Text strong underline>Six Degrees Challenge:</Text> choose an actor and a number of 
                     degrees you want to be challenged by, and we'll tell you who to connect them with!
                 </Paragraph>
                 <Paragraph>
-                    <Text strong underline>Filmography Challenge</Text> let's you choose an actor and we'll 
+                    <Text strong underline>Filmography Challenge:</Text> choose an actor and we'll 
                     test your knowledge of their filmography by making you select posters from the films they've been in. Good luck!
                 </Paragraph>
             </Typography>
