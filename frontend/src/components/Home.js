7b09c8e8bf0ea9b119ae2405fb13766bd7e1ea82
NickSpangler <Spangleyman@mac.com>
2020-12-23 23:55:30 -0500

    messy home component css

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Typography, Divider } from 'antd';
 import '../css/animista.css'
-import Logo from '../images/edited-logo.png'
+import Logo from '../images/edited-logo2.png'
 
 const { Title, Paragraph, Text} = Typography;
 
@@ -9,12 +9,17 @@ const { Title, Paragraph, Text} = Typography;
 export default function Home() {
     return (
         <div className='home-container'>
-            <br></br><br></br>
-            <div className='logo-container'>
-                <img src={ Logo } width='600px'></img>
-            </div>
-            <div>
-            {/* <Typography>
+            <div className='home-content'>
+            <br></br><br></br><br></br>
+                <div id="f1_container">
+                    <div id="f1_card" class="shadow">
+                        <div class="front face">
+                        <img src={ Logo } width='600px'></img>
+                        </div>
+                        <div class="back face center">
+                            {/* <p>This is nice for exposing more information about an image.</p>
+                            <p>Any content can go here.</p> */}
+            <Typography>
                 <Title>Welcome to Kevin Bacon Bits</Title>
                 <Paragraph>
                     At KBB we know the bits are just good as the whole strip... if not better!
@@ -32,7 +37,27 @@ export default function Home() {
                     <Text strong underline>Filmography Challenge</Text> let's you choose an actor and we'll 
                     test your knowledge of their filmography by making you select posters from the films they've been in. Good luck!
                 </Paragraph>
-            </Typography> */}
+            </Typography>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* <div className='logo-container heartbeat'>
+                <img src={ Logo } width='600px'></img>
+            </div>
+            <div> */}
+            {/* </div> */}
             </div>
         </div>
     )
