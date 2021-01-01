ede3cd6ba368808b85bd4ef8f0cbec82c0c515c3
NickSpangler <Spangleyman@mac.com>
2021-01-01 15:25:20 -0500

    DB from 400,000 entries

--- kbb-backend/db/seeds.rb
+++ kbb-backend/db/seeds.rb
@@ -1,7 +1,7 @@
 def getData
 
-    i = 249802
-    while i <= 300000 do
+    i = 400000
+    while i <= 500000 do
         response = RestClient.get("https://api.themoviedb.org/3/movie/#{i}?api_key=f561f9632b29613ae5d1646d3298a753&language=en-US"){|response, request, result| response }
         if response.code == 200
             json = JSON.parse response
