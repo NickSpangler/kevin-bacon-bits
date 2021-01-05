387fe8a7f2a0b5f0033aa3d305d41c9a82097622
NickSpangler <Spangleyman@mac.com>
2021-01-05 12:37:18 -0500

    fix issue of sometimes less than 5 possible_movies

--- kbb-backend/app/controllers/movies_controller.rb
+++ kbb-backend/app/controllers/movies_controller.rb
@@ -2,16 +2,33 @@ class MoviesController < ApplicationController
 
     def possible_movies
         actor = Actor.find(params[:actor_id])
-        
         current_movie = Movie.find(params[:current_movie])
-
-        cast = current_movie.actors[0...15].filter{ |a| a.id != actor.id }
+        cast = current_movie.actors.filter{ |a| a.id != actor.id }
         movies_that_year = []
-        cast.each do |a|
-            a.movies.each do |m|
-                if m.release_year != nil && m.release_year >= current_movie.release_year - 2 && m.release_year <= current_movie.release_year + 2 && !m.actors.include?(actor)
-                    movies_that_year << m
+
+        if current_movie.release_year == nil
+            cast.each do |a|
+                a.movies.each do |m|
+                    if !m.actors.include?(actor)
+                        movies_that_year << m
+                    end
                 end
+            end
+            until movies_that_year.length >= 4
+                extra_movie = Movie.find(rand(182697))
+                movies_that_year << extra_movie unless extra_movie.actors.include?(actor)
+            end
+        else
+            cast.each do |a|
+                a.movies.each do |m|
+                    if m.release_year != nil && m.release_year >= current_movie.release_year - 2 && m.release_year <= current_movie.release_year + 2 && !m.actors.include?(actor)
+                        movies_that_year << m
+                    end
+                end
+            end
+            until movies_that_year.length >= 4
+                extra_movie = Movie.find(rand(182697))
+                movies_that_year << extra_movie unless extra_movie.actors.include?(actor)
             end
         end
         possible_movies = movies_that_year.uniq.shuffle.slice(0,4)
