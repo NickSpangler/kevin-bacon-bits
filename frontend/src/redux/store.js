445c50c80ece8983817b45ea5f17c5140b095390
NickSpangler <Spangleyman@mac.com>
2021-01-06 16:51:55 -0500

    connect sixDegreesChallengeReducer to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,14 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import searchReducer from './reducers/searchReducer'
 import filmographyReducer from './reducers/filmographyReducer'
 import settingsReducer from './reducers/settingsReducer'
+import sixDegreesChallengeReducer from './reducers/sixDegreesChallengeReducer'
 import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
     search: searchReducer,
+    sixDegreesChallenge: sixDegreesChallengeReducer,
     filmography: filmographyReducer,
     settings: settingsReducer
 });
