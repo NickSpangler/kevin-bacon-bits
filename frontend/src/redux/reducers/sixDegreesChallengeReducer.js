63df18d2b5aa0047fc0edd2f96a4c020d5dda9d4
NickSpangler <Spangleyman@mac.com>
2021-01-06 16:46:25 -0500

    create sixDegreesChallengereducer

---
+++ frontend/src/redux/reducers/sixDegreesChallengeReducer.js
@@ -0,0 +1,17 @@
+function sixDegreesChallengeReducer(state = {
+    actor: 'not selected',
+    actor_movies: [],
+    current_movie: {},
+    possible_answers: [],
+    round_result: 'waiting',
+    history: [],
+    challenge_active: false,
+    total_history: 0
+}, action) {
+switch(action.type) {
+default:
+return state;
+}
+}
+
+export default sixDegreesChallengeReducer;
