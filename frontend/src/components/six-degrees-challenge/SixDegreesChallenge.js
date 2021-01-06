7c0953fc199f83dce98d573e13cec7076fd85385
NickSpangler <Spangleyman@mac.com>
2021-01-06 16:33:33 -0500

    remove placeholder code

--- frontend/src/components/six-degrees-challenge/SixDegreesChallenge.js
+++ frontend/src/components/six-degrees-challenge/SixDegreesChallenge.js
@@ -25,26 +25,4 @@ export default function SixDegreesChallenge() {
         </div>
         </>
     )
-
-    // return (
-    //     <div className='challenge-container'>
-    //     <div className='challenge-content'>
-    // //     <br></br><br></br><br></br>
-    // //     <div className='challenge-component slide-in-bottom'>
-    // //     <Result
-    //         status="404"
-    //         title="Sorry, we're still unboxing this feature."
-    //         subTitle="Check back later for updates, and enjoy another feature at this time."
-    //         extra={[<Link to='/six-degrees-search'>
-    //                     <Button type="primary">Six Degrees Search</Button>
-    //                 </Link>,
-    //                 <Link to='/filmography-challenge'>
-    //                     <Button type="primary">Filmography Challenge</Button>
-    //                 </Link>
-    //         ]}
-    //     />
-    //     </div>
-    //     </div>
-    //     </div>
-    // )
 }
