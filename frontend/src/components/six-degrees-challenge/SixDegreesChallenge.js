acb7051aa273cc393496e273016c6a34a1b1e423
NickSpangler <Spangleyman@mac.com>
2021-01-06 15:42:03 -0500

    Add titles to Six Degrees Challenge

--- frontend/src/components/six-degrees-challenge/SixDegreesChallenge.js
+++ frontend/src/components/six-degrees-challenge/SixDegreesChallenge.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { Result, Button } from 'antd';
+import { Result, Button, Space } from 'antd';
 import { Link } from 'react-router-dom';
 
 export default function SixDegreesChallenge() {
     return (
+        <>
         <div className='challenge-container'>
         <div className='challenge-content'>
-        <br></br><br></br><br></br>
-        <div className='challenge-component slide-in-bottom'>
-        <Result
-            status="404"
-            title="Sorry, we're still unboxing this feature."
-            subTitle="Check back later for updates, and enjoy another feature at this time."
-            extra={[<Link to='/six-degrees-search'>
-                        <Button type="primary">Six Degrees Search</Button>
-                    </Link>,
-                    <Link to='/filmography-challenge'>
-                        <Button type="primary">Filmography Challenge</Button>
-                    </Link>
-            ]}
-        />
+            <h1 style={{color: 'white'}}>Build the link between two actors:</h1>
+            <Space size='large'>
+            <div>
+                
+                <h3 style={{color: 'white', marginRight: '50px'}}>Select starting point:</h3>
+            </div>
+            <div>
+                <h3 style={{color: 'white', marginLeft: '50px'}}>Select degree of difficulty:</h3>
+            </div>
+            </Space>
         </div>
         </div>
-        </div>
+        </>
     )
+
+    // return (
+    //     <div className='challenge-container'>
+    //     <div className='challenge-content'>
+    // //     <br></br><br></br><br></br>
+    // //     <div className='challenge-component slide-in-bottom'>
+    // //     <Result
+    //         status="404"
+    //         title="Sorry, we're still unboxing this feature."
+    //         subTitle="Check back later for updates, and enjoy another feature at this time."
+    //         extra={[<Link to='/six-degrees-search'>
+    //                     <Button type="primary">Six Degrees Search</Button>
+    //                 </Link>,
+    //                 <Link to='/filmography-challenge'>
+    //                     <Button type="primary">Filmography Challenge</Button>
+    //                 </Link>
+    //         ]}
+    //     />
+    //     </div>
+    //     </div>
+    //     </div>
+    // )
 }
