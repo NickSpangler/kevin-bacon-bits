b020524057233c3adf74fef7c94ed917c8190f1f
NickSpangler <Spangleyman@mac.com>
2021-01-06 16:42:08 -0500

    add animated etrances to SixDegreeChallenge components

--- frontend/src/components/six-degrees-challenge/SixDegreesChallenge.js
+++ frontend/src/components/six-degrees-challenge/SixDegreesChallenge.js
@@ -11,11 +11,11 @@ export default function SixDegreesChallenge() {
         <div className='challenge-content'>
             <h1 style={{color: 'white'}}>Build the link between two actors:</h1>
             <Space size='large' direction="vertical">
-            <div>
+            <div className='slide-in-bottom'>
                 <StartingPoint />
                 {/* <h3 style={{color: 'white', marginRight: '50px'}}>Select starting point:</h3> */}
             </div>
-            <div>
+            <div className='slide-in-top'>
                 <br></br>
                 <h3 style={{color: 'white'}}>Select degree of difficulty:</h3>
                 <SelectDegree />
