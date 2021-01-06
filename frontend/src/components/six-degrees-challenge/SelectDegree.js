08aafe2f8f46d549c6e723ebc2e505e330c06e13
NickSpangler <Spangleyman@mac.com>
2021-01-06 16:11:54 -0500

    import radio buttons from Ant D

--- frontend/src/components/six-degrees-challenge/SelectDegree.js
+++ frontend/src/components/six-degrees-challenge/SelectDegree.js
@@ -4,7 +4,20 @@ import { Radio } from 'antd';
 export default function SelectDegree() {
     return (
         <div>
-            
+            <Radio.Group defaultValue="a" buttonStyle="solid">
+                <Radio.Button value="a">Hangzhou</Radio.Button>
+                <Radio.Button value="b">Shanghai</Radio.Button>
+                <Radio.Button value="c">Beijing</Radio.Button>
+                <Radio.Button value="d">Chengdu</Radio.Button>
+            </Radio.Group>
+            <Radio.Group defaultValue="c" buttonStyle="solid" style={{ marginTop: 16 }}>
+                <Radio.Button value="a">Hangzhou</Radio.Button>
+                <Radio.Button value="b" disabled>
+                Shanghai
+                </Radio.Button>
+                <Radio.Button value="c">Beijing</Radio.Button>
+                <Radio.Button value="d">Chengdu</Radio.Button>
+            </Radio.Group>
         </div>
     )
 }
