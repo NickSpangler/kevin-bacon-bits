451a87c33ad5ebde14c4aa258798aa7390fe5b42
NickSpangler <Spangleyman@mac.com>
2021-01-06 16:14:38 -0500

    set radio button values for SelectDegree

--- frontend/src/components/six-degrees-challenge/SelectDegree.js
+++ frontend/src/components/six-degrees-challenge/SelectDegree.js
@@ -5,18 +5,9 @@ export default function SelectDegree() {
     return (
         <div>
             <Radio.Group defaultValue="a" buttonStyle="solid">
-                <Radio.Button value="a">Hangzhou</Radio.Button>
-                <Radio.Button value="b">Shanghai</Radio.Button>
-                <Radio.Button value="c">Beijing</Radio.Button>
-                <Radio.Button value="d">Chengdu</Radio.Button>
-            </Radio.Group>
-            <Radio.Group defaultValue="c" buttonStyle="solid" style={{ marginTop: 16 }}>
-                <Radio.Button value="a">Hangzhou</Radio.Button>
-                <Radio.Button value="b" disabled>
-                Shanghai
-                </Radio.Button>
-                <Radio.Button value="c">Beijing</Radio.Button>
-                <Radio.Button value="d">Chengdu</Radio.Button>
+                <Radio.Button value="a">One Degree</Radio.Button>
+                <Radio.Button value="b">Two Degrees</Radio.Button>
+                <Radio.Button value="c">Three Degrees</Radio.Button>
             </Radio.Group>
         </div>
     )
