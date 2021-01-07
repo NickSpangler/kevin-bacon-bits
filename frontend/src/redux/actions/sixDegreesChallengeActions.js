f6309ce5a8b776468be32c9f716723abfd14efc0
NickSpangler <Spangleyman@mac.com>
2021-01-07 13:33:04 -0500

    define setDegree in SDChallengeActions

--- frontend/src/redux/actions/sixDegreesChallengeActions.js
+++ frontend/src/redux/actions/sixDegreesChallengeActions.js
@@ -7,3 +7,11 @@ export const setTargetA = (actor_id) => {
         })
     };
 }
+
+export const setDegree = (degree) => {
+    return (
+        {
+            type: 'SET_DEGREE', degree: degree
+        }
+    )
+}
