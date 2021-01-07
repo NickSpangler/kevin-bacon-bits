153fe2f4c02855dbbd7ebcf3856afcfa8eaa2038
NickSpangler <Spangleyman@mac.com>
2021-01-07 14:41:44 -0500

    remove counter_cache option from Actor model

--- kbb-backend/app/models/actor.rb
+++ kbb-backend/app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ApplicationRecord
     has_many :movie_actors
-    has_many :movies, :through => :movie_actors, counter_cache: true
+    has_many :movies, :through => :movie_actors
 
     # scope :auto_complete, -> (query) { where("name ILIKE (?)", "#{query}%" ).order(:name)}
     scope :auto_complete, -> (query) { where("name ILIKE (?)", "#{query}%" ).order("profile_path = '', name")}
