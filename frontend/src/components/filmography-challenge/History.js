722be50f50c7a84602ee27a7131d22336a61b6ee
NickSpangler <Spangleyman@mac.com>
2021-02-19 16:35:16 -0500

    indent code

--- frontend/src/components/filmography-challenge/History.js
+++ frontend/src/components/filmography-challenge/History.js
@@ -10,7 +10,7 @@ const History = (props) => {
                 <Divider orientation="center">
                 <Badge count={props.history.length} offset={[-55, 28]} style={{position: 'absolute', 'z-index': '10'}}>
                     Correct Answers  
-                    </Badge>
+                </Badge>
                 </Divider>
                     <Space size={[8, 16]} wrap>
                         {props.history.map(m => <div className='roll-in-right'><img src={`https://image.tmdb.org/t/p/w200${m.poster_path}`} height='130px'></img></div>)}
